51dfab5f80147d68deaab0b343929a5fae7445f1
mrszympek <piela.bartek@gmail.com>
2017-10-22 15:09:27 +0200

    Geolocation

--- app/index.js
+++ app/index.js
@@ -1,44 +1,47 @@
 const key = "f74df980fa4dfddbdd9152467484cea7";
 
-let getWeather = function(city) {
-	let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`; 
-
-	// console.log(url);
-
-	fetch(url, { method: "GET" })
-		.then(response => response.json())
-		.then(setData);
-
-	function setData(json) {
-		let temp = Math.round(((json.main.temp - 273.15) * 100) / 100).toFixed(1);
-		// let temp = json.main.temp - 273.15;
-		let temperature = document.querySelector("#temp");
-		temperature.innerHTML = temp;
-
-		let desc = json.weather[0].description;
-		let description = document.querySelector("#desc");
-		description.innerHTML = desc;
-
-		let latt = json.coord.lat;
-		let latitude = document.querySelector('#latitude');
-		latitude.innerHTML += latt;
-
-		let long = json.coord.lon;
-		let longitude = document.querySelector("#longitude");
-		longitude.innerHTML += long;
-
-
-	}
+function getData(city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(setData);
 }
 
-let button = document.querySelector("#form");
-button.addEventListener("submit", function(e) {
-	let city = document.querySelector("#city").value;
-	document.querySelector("#city-name").innerHTML = city;
-	// console.log(city);
-	getWeather(city);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setCurrentPosition);
+    console.log(navigator.geolocation)
+} else {
+    alert("geolocation is not supported");
+}
 
-	e.preventDefault();
-	return false;
-});
+function setCurrentPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${key}`
 
+    fetch(url)
+        .then(resp => resp.json())
+        .then(setData)
+}
+
+function setData(json) {
+    let city = document.querySelector("#city-name").innerHTML = json.name;
+
+    let temperature = document.querySelector("#temp").innerHTML = Math.round(((json.main.temp - 273.15) * 100) / 100).toFixed(1);
+    
+    let description = document.querySelector("#desc").innerHTML = json.weather[0].description;
+
+    let latitude = document.querySelector("#latitude").innerHTML = json.coord.lat;
+
+    let longitude = document.querySelector("#longitude").innerHTML = json.coord.lon;
+}
+
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", function(e) {
+    let city = document.querySelector("#city").value;
+    getData(city);
+
+    e.preventDefault();
+    return false;
+})
