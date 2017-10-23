b5c979d4e925921bfed49284342d54be1e434d47
piela.bartek@gmail.com <piela.bartek@gmail.com>
2017-10-23 10:14:09 +0200

    weather images

--- app/index.js
+++ app/index.js
@@ -27,13 +27,33 @@ function setCurrentPosition(position) {
 function setData(json) {
     let city = document.querySelector("#city-name").innerHTML = json.name;
 
-    let temperature = document.querySelector("#temp").innerHTML = Math.round(((json.main.temp - 273.15) * 100) / 100).toFixed(1);
+    let temperature = document.querySelector("#temp").innerHTML = Math.round(((json.main.temp - 273.15) * 100) / 100).toFixed(1) + "°C";
     
     let description = document.querySelector("#desc").innerHTML = json.weather[0].description;
 
     let latitude = document.querySelector("#latitude").innerHTML = json.coord.lat;
 
     let longitude = document.querySelector("#longitude").innerHTML = json.coord.lon;
+    
+    let img = document.querySelector("#img");
+
+    switch (description) {
+        case "light rain":
+            img.src = 'https://cdn4.iconfinder.com/data/icons/wthr-color/32/cloud-drizzle-512.png';
+            break;
+            
+        case "clear sky":
+            img.src = 'https://cdn0.iconfinder.com/data/icons/weather-web-app-ui/100/weather-22-512.png'
+            break;
+
+        case "broken clouds":
+            img.src = 'https://maxcdn.icons8.com/Share/icon/Weather//clouds1600.png'
+            break;
+
+        case "mist":
+            img.src = 'https://imageog.flaticon.com/icons/png/512/15/15774.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
+            break;
+        }
 }
 
 let form = document.querySelector("#form");
